f75b0560f5f10e36fd5af077b4d9f0e111874578
Jamie <jamie.m.pena@gmail.com>
2018-04-11 21:31:57 -0400

    jp

--- react-app/components/Shared/Navbar/Navbar.js
+++ react-app/components/Shared/Navbar/Navbar.js
@@ -24,6 +24,7 @@ class Navbar extends Component {
 <div class="nav-wrapper">
       <a href="#!" class="brand-logo center"><i class="material-icons"></i>Animedia</a>
       <ul class="right hide-on-med-and-down">
+        {/*<li><a href="sass.html"><i class="material-icons">search</i></a></li>*/}
         <li><input type="text" name="search" placeholder="Search.."></input></li>
         <li><a href="badges.html"><i class="material-icons">view_module</i></a></li>
         <li><a href="collapsible.html"><i class="material-icons">refresh</i></a></li>
