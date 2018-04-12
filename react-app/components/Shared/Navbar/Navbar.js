3e72611f059db34440457820345d224073a31d38
Jamie <jamie.m.pena@gmail.com>
2018-04-11 21:13:35 -0400

    jamie push added search bar and icons

--- react-app/components/Shared/Navbar/Navbar.js
+++ react-app/components/Shared/Navbar/Navbar.js
@@ -11,17 +11,27 @@ class Navbar extends Component {
     return (
       <div>
         <nav className="sugabums">
-          <div className = "nav-wrapper">
+          {/*<div className = "nav-wrapper">
             <a href="/" className="brand-logo center">Animedia</a>
-            <ul id="nav-mobile" className="left hide-on-med-and-down">
-              <li><a href ="">About</a></li>
-              <li><a href ="">About</a></li>
+            <ul class="right hide-on-med-and-down">
+              <li><a href="sass.html"><i className="material-icons"></i></a></li>
+              <li><a href="badges.html"><i className="material-icons">view_module</i></a></li>
+              <li><a href="collapsible.html"><i className="material-icons">refresh</i></a></li>
+              <li><a href="mobile.html"><i className="material-icons">more_vert</i></a></li>
             </ul>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li><a href ="">About</a></li>
-              <li><a href ="">About</a></li>
-            </ul>
-          </div>
+          </div>*/}
+
+<div class="nav-wrapper">
+      <a href="#!" class="brand-logo center"><i class="material-icons"></i>Animedia</a>
+      <ul class="right hide-on-med-and-down">
+        <li><input type="text" name="search" placeholder="Search.."></input></li>
+        <li><a href="badges.html"><i class="material-icons">view_module</i></a></li>
+        <li><a href="collapsible.html"><i class="material-icons">refresh</i></a></li>
+        <li><a href="mobile.html"><i class="material-icons">more_vert</i></a></li>
+      </ul>
+    </div>
+
+
         </nav>
       </div>
     );
