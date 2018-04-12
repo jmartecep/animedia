e83c641f61968d0db1c48aa35f14857887567a47
Jamie <jamie.m.pena@gmail.com>
2018-04-11 22:02:45 -0400

    jp last push of the night

--- react-app/components/Shared/Navbar/Navbar.js
+++ react-app/components/Shared/Navbar/Navbar.js
@@ -23,14 +23,19 @@ class Navbar extends Component {
 
 <div class="nav-wrapper">
       <a href="#!" class="brand-logo center"><i class="material-icons"></i>Animedia</a>
+      
       <ul class="right hide-on-med-and-down">
-        {/*<li><a href="sass.html"><i class="material-icons">search</i></a></li>*/}
-        <li><input type="text" name="search" placeholder="Search.."></input></li>
-        <li><a href="badges.html"><i class="material-icons">view_module</i></a></li>
-        <li><a href="collapsible.html"><i class="material-icons">refresh</i></a></li>
-        <li><a href="mobile.html"><i class="material-icons">more_vert</i></a></li>
+          <li>
+          <form>
+            <div class="input-field">
+              <input id="search" type="search" required></input>
+              <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+              <i class="material-icons">close</i>
+            </div>
+          </form>
+          </li>
       </ul>
-    </div>
+</div>{/*End Nav Wrapper*/}
 
 
         </nav>
