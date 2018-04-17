6cedf6fbb2677a245cd5874aa47f4f00dfbccbaa
Jamie Pena <jamie.m.pena@gmail.com>
2018-04-17 19:03:21 -0400

    test

--- react-app/components/Shared/Navbar/Navbar.js
+++ react-app/components/Shared/Navbar/Navbar.js
@@ -1,16 +1,17 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "materialize-css/dist/css/materialize.min.css";
-import "./navbar.css";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import 'materialize-css/dist/css/materialize.min.css'; 
+import './navbar.css';
+
 
 class Navbar extends Component {
-  render() {
+  render () {
     return (
       <div>
-        <nav>
+        <nav className="sugabums">
           {/*<div className = "nav-wrapper">
             <a href="/" className="brand-logo center">Animedia</a>
-            <ul className="right hide-on-med-and-down">
+            <ul class="right hide-on-med-and-down">
               <li><a href="sass.html"><i className="material-icons"></i></a></li>
               <li><a href="badges.html"><i className="material-icons">view_module</i></a></li>
               <li><a href="collapsible.html"><i className="material-icons">refresh</i></a></li>
@@ -18,26 +19,23 @@ class Navbar extends Component {
             </ul>
           </div>*/}
 
-          <div className="nav-wrapper">
-            <a href="#!" className="brand-logo center">
-              <i className="material-icons" />Animedia
-            </a>
+<div class="nav-wrapper">
+      <a href="#!" class="brand-logo center"><i class="material-icons"></i>Animedia</a>
+      
+      <ul class="right hide-on-med-and-down">
+          <li>
+          <form>
+            <div class="input-field">
+              <input id="search" type="search" required></input>
+              <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+              <i class="material-icons">close</i>
+            </div>
+          </form>
+          </li>
+      </ul>
+</div>{/*End Nav Wrapper*/}
 
-            <ul className="right hide-on-med-and-down">
-              <li>
-                <form>
-                  <div className="input-field">
-                    <input id="search" type="search" required />
-                    <label className="label-icon" htmlFor="search">
-                      <i className="material-icons">search</i>
-                    </label>
-                    <i className="material-icons">close</i>
-                  </div>
-                </form>
-              </li>
-            </ul>
-          </div>
-          {/*End Nav Wrapper*/}
+
         </nav>
       </div>
     );
