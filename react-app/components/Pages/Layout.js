624df51cc64a73bac5ae9f756a2b891c5cd26950
Jamie Pena <jamie.m.pena@gmail.com>
2018-04-17 19:41:06 -0400

    test5

--- react-app/components/Pages/Layout.js
+++ react-app/components/Pages/Layout.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Navbar from "../Shared/Navbar";
+import Footer from "../Shared/Footer";
 
 class Layout extends Component {
   render() {
     return (
       <section>
         <Navbar />
+        <Footer />
         {this.props.children}
       </section>
     );
