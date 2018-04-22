a6be2a76edaa843ba1f9deda8089ffcd06920b3f
Sean Kelly <spkellydev@gmail.com>
2018-04-21 21:40:12 -0400

    porductionized

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const next = require("next");
 
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV == "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const PORT = process.env.PORT || 3000
